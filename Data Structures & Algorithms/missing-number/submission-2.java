77a67ef77326ee7651196d01aa5c747c2859ccd8
gauravmalik04 <malik07x24@gmail.com>
2026-09-05 11:25:56 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++) if(nums[i]!=i) return i;
+        return nums[n-1]+1;
+    }
+}
